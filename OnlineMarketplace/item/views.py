1af95d74e67c61bee17b1c39e69fe4e9d04cef17
Sai-Deepak-1 <psaideepak1@gmail.com>
2024-01-25 14:14:14 +0530

    Added new Changes in Code

--- OnlineMarketplace/item/views.py
+++ OnlineMarketplace/item/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Item
 
-# Create your views here.
+def detail(request, param_key):
+    item = get_object_or_404(Item, pk = param_key)
+    
+    return render(request, 'item/detail.html', {
+        'item' : item
+    })
