6dd9815d070acbae3ea964aa816d5ea229415ed4
Sai-Deepak-1 <psaideepak1@gmail.com>
2024-02-02 21:32:34 +0530

    Fixed an Error

--- OnlineMarketplace/item/urls.py
+++ OnlineMarketplace/item/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'item'
+
 urlpatterns = [
-    path("<int:pk/>", views.detail, name="details"),
+    path("item/<int:pk>/", views.details, name = 'details' ),
 ]
--- OnlineMarketplace/item/views.py
+++ OnlineMarketplace/item/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Item
 
-def detail(request, param_key):
-    item = get_object_or_404(Item, pk = param_key)
+def details(request, pk):
+    item = get_object_or_404(Item, id = pk)
     
     return render(request, 'item/detail.html', {
         'item' : item
