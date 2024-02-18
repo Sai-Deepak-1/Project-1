62d3aef90c914d32a66333e0e2fbd00437a5e517
Sai-Deepak-1 <psaideepak1@gmail.com>
2024-02-18 11:27:35 +0530

    Update URLs and templates, added related items to item detail view

--- OnlineMarketplace/OnlineMarketplace/urls.py
+++ OnlineMarketplace/OnlineMarketplace/urls.py
@@ -6,8 +6,7 @@ from django.urls import path, include
 from core.views import index, contact
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("item/", include('item.urls')),
-    path("contact/", contact, name="contact"),
-    path("admin/", admin.site.urls),
+    path('', include('core.urls')),
+    path('item/', include('item.urls')),
+    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
